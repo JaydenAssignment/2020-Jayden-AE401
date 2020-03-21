65aedd72333ced9e1fa42a5158a5f049ca729875
JaydenAssignment <61931041+JaydenAssignment@users.noreply.github.com>
2020-03-21 12:26:39 +0800

    AE401-2-Score
    有沒有及格

---
+++ Score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 11:47:10 2020
+
+@author: jaydenjian
+"""
+score=input("請輸入成績：")
+
+score=int(score)
+
+if score>60:
+    print("及格！")
+else:
+    print("不及格！")
