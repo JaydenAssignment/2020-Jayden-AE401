f94ce636c7fa9ff4dba8aa213587da32874a093f
JaydenAssignment <61931041+JaydenAssignment@users.noreply.github.com>
2020-05-23 15:52:54 +0800

    Homework1
    使用者輸入圓的半徑，
    電腦在螢幕上回答圓周長、圓面積。

---
+++ Homework1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 09:09:11 2020
+
+@author: jaydenjian
+"""
+
+radius=input("請輸入半徑：")
+radius=float(radius)
+circle=2*3.14*radius
+area=3.14*radius**2
+print("圓周長＝",circle)
+print("圓面積＝",area)
+
