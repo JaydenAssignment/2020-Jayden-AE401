b29a092802baee2e3c300fca42a3ff26ad3f56c1
JaydenAssignment <61931041+JaydenAssignment@users.noreply.github.com>
2020-07-18 22:42:37 +0800

    Add files via upload

---
+++ Unicode轉換範例.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 10:55:15 2020
+
+@author: jaydenjian
+"""
+
+myString=ord("嗨")
+print(myString)
+
+
+
+
+
+myUnicode=chr(21992)
+print(myUnicode)
